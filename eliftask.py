948c65948827cfff30ea8f7362e630af723bec59
akshayarmca <105848584+akshayarmca@users.noreply.github.com>
2022-05-19 14:42:34 +0530

    Update eliftask.py

--- eliftask.py
+++ eliftask.py
@@ -1,6 +1,7 @@
 name=input("enter your name:")
-dob=int(input("enter your dob(DDMM):"))
-if name=='ela' and dob=='0509'
+dob=int(input("enter your dob(DM) ex 12:"))
+print (dob)
+if name == 'ela' and dob == 59:
     print("hello buddy!how are you")
     for i in range(1,4):
         print("your mom name is chitra")
