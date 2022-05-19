40a84e488596791e75506097a98267ca8994154e
akshayarmca <105848584+akshayarmca@users.noreply.github.com>
2022-05-19 14:10:26 +0530

    Create eliftask.py

---
+++ eliftask.py
@@ -0,0 +1,18 @@
+name=input("enter your name:")
+dob=int(input("enter your dob(DDMM):"))
+if name=='ela' and dob=='0509'
+    print("hello buddy!how are you")
+    for i in range(1,4):
+        print("your mom name is chitra")
+        print("your dada name is mano")
+elif name=='akshaya':
+    print("hello buddy!how are you")
+    for i in range(1,4):
+        print("your mom name is selvi")
+elif name=='kashvika':
+    print("hello buddy!how are you ")
+    for j in range(1,4):
+        print("your dad name is ela")
+    
+else:
+    print("i am not able to regconize who are you?")
