dd020cd96d4c948c0b1134fcedd001741a7ac132
akshayarmca <105848584+akshayarmca@users.noreply.github.com>
2022-05-19 13:53:28 +0530

    Create mytest.py

---
+++ mytest.py
@@ -0,0 +1,16 @@
+name=input("enter your name:")
+if name=='ela':
+    print("hello buddy!how are you")
+    for i in range(1,4):
+        print("your mom name is chitra")
+        print("your dada name is mano")
+elif name=='akshaya':
+    print("hello buddy!how are you")
+    for i in range(1,4):
+        print("your mom name is selvi")
+elif name=='kashvika':
+    print("hello buddy!how are you ")
+    for j in range(1,4):
+        print("your dad name is ela")
+else:
+    print("i am not able to regconize who are you?")
